92c0604c08fce8b517df35b20a5deda74b2561de
kesotof <ke.sotof@duocuc.cl>
2025-04-19 18:10:00 -0400

    Agregado de vistas

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,47 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SidebarComponent } from './shared/components/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [
+    RouterOutlet,
+    SidebarComponent
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = 'frontend';
-  backendStatus = "loading...";
-  databaseStatus = "loading...";
+  isCollapsed = false;
 
-  // ngOnInit is called after Angular initializes the component
-  async ngOnInit() {
-    try {
-      const healthData = await backendHealthCheck();
-      // Update properties once data is fetched
-      this.backendStatus = healthData['backend-status'];
-      this.databaseStatus = healthData['database-status'];
-      console.log('Health Check Data:', healthData); // For testing
-    } catch (error) {
-      console.error('Error fetching health check:', error);
-      this.backendStatus = 'error';
-      this.databaseStatus = 'error';
-    }
+  onSidebarToggle(collapsed: boolean) {
+    this.isCollapsed = collapsed;
   }
-
-}
-
-async function backendHealthCheck(): Promise<any> {
-  // Use the correct health check endpoint URL if needed (e.g., /api/health/)
-  const response = await fetch('http://localhost:8100/', {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  });
-
-  if (!response.ok) {
-    // Throw an error with more context if possible
-    const errorText = await response.text();
-    throw new Error(`Network response was not ok (${response.status}): ${errorText}`);
-  }
-  return response.json(); // Parse the JSON response
 }
--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { ProductAddComponent } from './pages/product-add/product-add.component';
+import { ProductListComponent } from './pages/product-list/product-list.component';
+import { CategoriesComponent} from './pages/categories/categories.component';
+import { ProductComponent } from './pages/product/product.component';
+import { OrdersComponent } from './pages/orders/orders.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+  { path: 'product-list', component: ProductListComponent },
+  { path: 'product-add', component: ProductAddComponent },
+  { path: 'categories', component: CategoriesComponent },
+  { path: 'product', component: ProductComponent },
+  { path: 'orders', component: OrdersComponent },
+  { path : '', redirectTo: 'product', pathMatch: 'full' },
+];
+
+
