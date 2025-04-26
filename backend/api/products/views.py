06df8c305922c91f1505c9a4f38e9fd8042e67a1
Eduardo Kusar <21264573+EduardoKQ@users.noreply.github.com>
2025-04-25 23:14:44 -0400

    refactor category api views

--- backend/api/products/views.py
+++ backend/api/products/views.py
@@ -41,10 +41,23 @@ def category_list_create(request):
         return process_category_list_create_post(request)
 
 
+# !!!
 @api_view(["GET", "PUT", "DELETE"])
-@permission_classes([IsAuthenticated])
 def category_get_update_delete(request, category_code):
-    return JsonResponse({"message": "category_get_update_delete"})
+    ##public endpoint for all users
+    if request.method == "GET":
+        return process_category_get(request, category_code)
+    # only store admin can update or delete categories
+    if request.method == "PUT" or "DELETE":
+        allowed_roles = [WebRoleNames.ADMIN_TIENDA]
+        auth_response = check_auth_allowed_role(request, allowed_roles)
+        if auth_response is not None:
+            return auth_response
+    # process the request
+    if request.method == "PUT":
+        return process_category_update(request, category_code)
+    if request.method == "DELETE":
+        return process_category_delete(request, category_code)
 
 
 ### helpers
@@ -85,3 +98,28 @@ def process_category_list_create_post(request):
         return JsonResponse(serializer.errors, status=400)
     except Exception as e:
         return JsonResponse({"error": str(e)}, status=500)
+
+
+def process_category_get(request, category_code):
+    # get a category by code
+    try:
+        categories = Category.objects.filter(category_code=category_code)
+        if not categories.exists():
+            return JsonResponse({"error": "Category not found"}, status=404)
+        categories_serializer = CategoriesDetailSerializer(categories, many=True)
+        return JsonResponse(categories_serializer.data, safe=False, status=200)
+
+    except Category.DoesNotExist:
+        return JsonResponse({"error": "Categories not found"}, status=404)
+    except Exception as e:
+        return JsonResponse({"error": str(e)}, status=500)
+
+
+#!!!
+def process_category_update(request, category_code):
+    return JsonResponse({"update": category_code})
+
+
+#!!!
+def process_category_delete(request, category_code):
+    return JsonResponse({"delete": category_code})
