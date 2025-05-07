41bfdfe85bcb2027ae83c18bf984f37542114eaa
kesotof <ke.sotof@duocuc.cl>
2025-05-07 14:34:14 -0400

    marca

---
+++ frontend/src/app/store/catalogo-marca/catalogo-marca.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-catalogo-marca',
+  imports: [],
+  templateUrl: './catalogo-marca.component.html',
+  styleUrl: './catalogo-marca.component.scss'
+})
+export class CatalogoMarcaComponent {
+
+}
